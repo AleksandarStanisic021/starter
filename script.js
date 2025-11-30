342c3ada1d4b17cfe0740da5b55508e5d221f406
ciandre1976 <ciandre23@gmail.com>
2025-11-30 17:51:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+    console.log('Hello, World!');
+    let amazing = true;
+    if (amazing) alert('JavaScript is FUN!');
+
+    console.log(40 + 8 + 23 - 10);
+    console.log('Jonas');
+    console.log(23);    
+    
