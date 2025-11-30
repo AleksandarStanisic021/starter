a44a691b54318b3d5f0823d7f69ea3810d84c2db
ciandre1976 <ciandre23@gmail.com>
2025-11-30 19:51:37 +0100

    Add person object and variable y with console logs

--- script.js
+++ script.js
@@ -6,6 +6,25 @@
     console.log(x);
     let firstName = "Alice"; /* Another multi-line comment */
     console.log("Name:", firstName); // Print the name
+  
+    const person= {
+        /* Person's details */
+        name: "Bob",        
+        age: 30 // Person's age
+    };
+    console.log(person);
+    person.age += 1; /* Increment age */
+    console.log(person);
+
+  var y;
+    console.log(y);
+
+    y = 20; // Assign value to y`   
+    console.log(y);
+
+
+
+
 
 
 
